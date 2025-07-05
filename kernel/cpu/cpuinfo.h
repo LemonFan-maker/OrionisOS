566f544589fc0709295511d28cf4930841fc0048
LemonFan-maker <2254650260@qq.com>
2025-07-05 15:21:14 +0800

    add the functionality to retrieve CPU cache information.

--- kernel/cpu/cpuinfo.h
+++ kernel/cpu/cpuinfo.h
@@ -9,3 +9,6 @@ void get_cpu_brand_string(char* buffer);
 
 // 获取 CPU 功能特性
 void get_cpu_features(uint32_t* ecx, uint32_t* edx);
+
+// 获取 CPU 各级缓存大小（单位：KB），如不支持返回0
+void get_cpu_cache_info(uint32_t* l1_kb, uint32_t* l2_kb, uint32_t* l3_kb);
